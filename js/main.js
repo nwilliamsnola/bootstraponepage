c1cf317ecc07aa0c9801eb9b822947ff2732f37f
Sandy Ludosky <sandyludosky@gmail.com>
2014-10-11 13:01:01 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function() {
+	var height = $(window).height();
+
+    $('#header').css('height', height);
+	   
+
+    //Scrollspy plugin
+    $('body').scrollspy({ target: '.navbar-spy'});
+
+    //Tooltip plugin
+    $('.socials').tooltip('toggle');
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+});
